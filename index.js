c56dca0dc49a3c8ba4fe3ea52b5c24e31022dd58
ADiksIT <30493897+ADiksIT@users.noreply.github.com>
2020-08-17 18:23:14 +0300

    fix: get default

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const mongoURL = process.env.mongoURL;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,21 +15,26 @@ app.use('/api/todoes', require('./routes/todoes/todoes.routes'));
 app.use('/api/add', require('./routes/add/add.routes'));
 app.use('/api/change', require('./routes/change/change.routes'));
 app.use('/api/delete', require('./routes/delete/delete.routes'));
+app.get('/', async (req, res) => {
+	res.send(
+		`App has been start ${PORT}, it's api, app: https://adiksit.github.io/todo-list/`
+	);
+});
 
 const start = async () => {
-  try {
-    await mongoose.connect(mongoURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    app.listen(PORT, () =>
-      console.log(`Server has been started port ${process.env.PORT}`),
-    );
-  } catch (e) {
-    console.log('Error: ', e.message);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(mongoURL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		app.listen(PORT, () =>
+			console.log(`Server has been started port ${process.env.PORT}`)
+		);
+	} catch (e) {
+		console.log('Error: ', e.message);
+		process.exit(1);
+	}
 };
 
 start();
